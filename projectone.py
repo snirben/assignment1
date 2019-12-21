766bdc9d1cff4ed1a872523a5f0f35d64339d89d
evghega <evghega@ac.sce.ac.il>
2019-12-21 19:47:08 +0200

    student class

--- projectone.py
+++ projectone.py
@@ -1,10 +1,31 @@
-class Person:
+import math
+class Person():
     def __init__(self, name, id, age):
         self.name = name
         self.id = id
+        self.age = age
+
+
+
+
+
+
+
+class Employee(Person):
+    def __init__(self, salary):
+        self.salary = salary
 
 
 class Student(Person):
     def _init_(self, avrg, ins):
         self.average = avrg
         self.institute = ins
+
+
+
+def main():
+    size = int(input('Enter size for arr'))
+    arr = []
+    for i in range(size):
+        type = input("Enter type of person(Studen , Employee,WorkingStudent):")
+
